94ba3f0fd957550fb66f0d807b61cf6b4ee23be4
quan1997ap <quan1997ap@gmail.com>
2019-06-04 12:32:58 +0700

    merge vs dev __

--- database/migrations/2019_06_04_050151_insert-data.php
+++ database/migrations/2019_06_04_050151_insert-data.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-
+// merge vs dev
 class InsertData extends Migration
 {
-    /**
+    /**`
      * Run the migrations.
      *
      * @return void
