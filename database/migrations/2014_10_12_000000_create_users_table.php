d9bbb56b9c3c050a3512792cf8698ac03fa798d0
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-04 14:46:38 +0700

    aaa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -57,7 +57,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->integer('account_id')->unsigned();; //accountId
             $table->string('type')->default('text'); //text, image... 
-            $table->string('content');
+            $table->string('content'); 
             $table->integer('task_id')->unsigned();; //taskId
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
