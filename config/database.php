1c586536e10c10bb417f3fd4c04d5a2c42a2af4b
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-07 11:19:57 +0700

    heroku

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'db4free.net'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3306'), 
             'database' => env('DB_DATABASE', 'todoappws from_db4free'),
             'username' => env('DB_USERNAME', 'balo110444'),
             'password' => env('DB_PASSWORD', 'nblong1997'),
