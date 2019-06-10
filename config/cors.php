270aa54a252557dc4c6189c6151f7783bbf96c0f
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-10 12:20:11 +0700

    aaaaaaa

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['http://localhost:3000'],
+    'allowedOrigins' => ['http://localhost:3000/','https://happydevapp.herokuapp.com/', 'https://happydevapp.herokuapp.com'],
     'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Accept'],
     'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
     'exposedHeaders' => [],
