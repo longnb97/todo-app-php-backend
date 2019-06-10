8ef8036dcfc70754f2d9cd9f0f04017c3d25f8c6
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-10 11:45:45 +0700

    fix corss

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,12 +62,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-<<<<<<< HEAD
-        'jwt-auth' => \App\Http\Middleware\JwtMiddleware::class
-=======
         'jwt-auth' => \App\Http\Middleware\jwtMiddleware::class,
         'cors' => \App\Http\Middleware\Cors::class,
->>>>>>> long
     ];
 
     /**
