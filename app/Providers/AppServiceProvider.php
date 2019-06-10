29b4e59954b1967f1bd2d85281c402291026094c
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-10 18:46:08 +0700

    fix chut url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
