0bc5cc743a428d40347c7666fecbe5e994fa7e6f
nguyenbaolong1197@gmail.com <nguyenbaolong1197@gmail.com>
2019-06-10 18:49:15 +0700

    fix chut url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +27,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot() 
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
     }
 }
